b240c5d8bd9d3c6f7880933f31c0d51ef345e5f1
Sasha Bilton <sasha@illusionaryterrain.com>
2024-01-03 10:17:39 +0000

    Display combat results

--- src/main/java/com/illusionaryterrain/adventure/rpg/CombatResult.java
+++ src/main/java/com/illusionaryterrain/adventure/rpg/CombatResult.java
@@ -1,5 +1,10 @@
 package com.illusionaryterrain.adventure.rpg;
 
+import com.illusionaryterrain.adventure.Out;
+import com.illusionaryterrain.adventure.Player;
+
+import static com.illusionaryterrain.adventure.rpg.Rules.STAMINA;
+
 public class CombatResult {
 
     public Integer aRoll;
@@ -9,6 +14,26 @@ public class CombatResult {
     public Integer bRoll;
     public Integer bTotal;
     public Integer bDamage;
+
+
+    public void displayResults(Player player, RPGSheet creature) {
+        Out.ln("You rolled a "+aRoll+" and the "+creature.getName()+" rolled a "+ bRoll);
+        Out.ln("Your total is "+aTotal+" while it's is "+bTotal);
+        if (aDamage > 0) {
+            Out.ln("You take "+aDamage+" damage!");
+        } else if(bDamage>0) {
+            Out.ln("It takes "+bDamage+" damage.");
+        } else {
+            Out.ln("You exchange blows but neither wound each other.");
+        }
+        if (creature.stats.get(STAMINA) < 1) {
+            Out.ln("You have slain the "+creature.name+"!");
+            player.location.killCreature(creature);
+        } if (player.rpg.stats.get(STAMINA) < 1) {
+            Out.ln("The "+creature.name+" has killed you!");
+            player.die();
+        }
+    }
 
 
 
